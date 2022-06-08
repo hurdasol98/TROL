100878231ff907ef30a1c0b80953d12ce2e8cd03
drin0131 <cpt0131@naver.com>
2022-06-08 14:33:57 +0900

    Edit Image and Json, RoleGameView

--- TROL/TROL/RoleGameView.swift
+++ TROL/TROL/RoleGameView.swift
@@ -9,7 +9,57 @@ import SwiftUI
 
 struct RoleGameView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image("logo-big")
+                .resizable()
+                .frame(width: 249, height: 86, alignment: .center)
+                .padding(.top,48)
+            
+            Text("게임을 통해 역할을 정해요!")
+                .font(.body)
+                .padding(12)
+            
+            Spacer()
+            
+            VStack{
+                HStack{
+                    
+                    Button("사다리타기"){
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }.frame(width: 170, height: 150)
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.yellow))
+                    Button("젓가락 뽑기"){
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }.frame(width: 170, height: 150)
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.yellow))
+                }
+                
+                HStack{
+                    Button("반사속도"){
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }.frame(width: 170, height: 150)
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.yellow))
+                    Button("무한탭"){
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }.frame(width: 170, height: 150)
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.yellow))
+                }
+                
+                HStack{
+                    Button("업다운"){
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }.frame(width: 170, height: 150)
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.yellow))
+                    Button("키워드"){
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }.frame(width: 170, height: 150)
+                        .background(RoundedRectangle(cornerRadius: 10).fill(Color.yellow))
+                }
+            }
+            
+            Spacer()
+            
+        }
     }
 }
 
