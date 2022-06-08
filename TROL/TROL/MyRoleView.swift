8a417b5a69d140fa492d8810764b291da65c9eb0
HyeS00 <67789254+HyeS00@users.noreply.github.com>
2022-06-08 14:36:28 +0900

    myrole

--- TROL/TROL/MyRoleView.swift
+++ TROL/TROL/MyRoleView.swift
@@ -9,7 +9,26 @@ import SwiftUI
 
 struct MyRoleView: View {
     var body: some View {
-        Text("나의 여행")
+        VStack(alignment: .leading) {
+            Text("나의 여행티켓").font(.custom("해피니스 산스", size: 28)).bold()
+            ZStack{
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Color.yellow)
+                    .frame(width: 354, height: 174)
+                VStack{
+                    Text("졸업여행")
+                    Text("2022.06.26~2022.07.01")
+                    HStack{
+                        Text("이름")
+                        Text("역할")
+                    }
+                    HStack{
+                        Text("바밤바")
+                        Text("총무")
+                    }
+                }
+            }
+        }
     }
 }
 
