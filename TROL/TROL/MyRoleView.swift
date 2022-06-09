99e3c84a2996116811f464e1c57adc809d6a35ef
hurdasol98 <73656470+hurdasol98@users.noreply.github.com>
2022-06-09 14:48:57 +0900

    Revert "Resolve Merge Conflict"
    This reverts commit 61b7fe9b4a5e94508d1b99210df7393d521197b5, reversing
    changes made to 6819e6df2cd214ec0b1debd06068a82f1155d28a.

--- TROL/TROL/MyRoleView.swift
+++ TROL/TROL/MyRoleView.swift
@@ -1,5 +1,5 @@
 //
-//  TabBarView.swift
+//  MyRoleView.swift
 //  TROL
 //
 //  Created by MBSoo on 2022/06/07.
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct TabBarView: View {
+struct MyRoleView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct TabBarView_Previews: PreviewProvider {
+struct MyRoleView_Previews: PreviewProvider {
     static var previews: some View {
-        TabBarView()
+        MyRoleView()
     }
 }
