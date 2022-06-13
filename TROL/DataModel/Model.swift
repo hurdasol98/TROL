d8ef6d1c8d0787b47060239082f4672935fe5b84
ChickenJoah <kpl88@naver.com>
2022-06-13 16:24:20 +0900

    fix: fix data model

--- TROL/DataModel/Model.swift
+++ TROL/DataModel/Model.swift
@@ -7,30 +7,92 @@
 
 import Foundation
 
-class Travel {
-    var name: String
-    var startTime: Date
-    var endTime: Date
-    var users: [User]
-    var travelCode: String
+// 전체적인 Travel 관리
+final class TravelData: ObservableObject {
+    @Published var travel: Travel = Travel(isExist: false, name: "", startDate: Date(), endDate: Date(), users: [User(name: "오션", myRole: nil, toDoList: nil)], usingRoles: [], travelCode: "")
     
-    init(name: String, startTime: Date, endTime: Date, users: [User]) {
-        self.name = name
-        self.startTime = Date()
-        self.endTime = Date()
-        self.users = []
-        self.travelCode = ""
+    func saveTravel(isExist: Bool, name: String, startDate: Date, endDate: Date, usingRoles: [Role]) {
+        self.travel.isExist = true
+        self.travel.name = name
+        self.travel.startDate = startDate
+        self.travel.endDate = endDate
+        self.travel.usingRoles = usingRoles
     }
+}
+
+// 앱이 기본적으로 제공해주는 역할 모음 + 커스텀 추가 가능
+final class RoleData: ObservableObject {
+    @Published var roles: [Role] = Role.defaultRoles
+}
+
+struct Travel {
+    var isExist: Bool
+    var name: String
+    var startDate: Date
+    var endDate: Date
+    var users: [User]
+    var usingRoles: [Role]
+    var travelCode: String
+}
+    
+//    mutating func saveTravel(isExist: Bool, name: String, startDate: Date, endDate: Date, usingRoles: [Role]) {
+//        self.isExist = true
+//        self.name = name
+//        self.startDate = startDate
+//        self.endDate = endDate
+//        self.usingRoles = usingRoles
+//    }
+
+//class Travel: ObservableObject {
+//    var isExist: Bool
+//    var users: [User]
+//    var name: String
+//    var startDate: Date
+//    var endDate: Date
+//    var usingRoles: [Role]
+//    var travelCode: String
+//
+//    init(isExist: Bool = false, name: String, startDate: Date, endDate: Date, users: [User], usingRoles: [Role], travelCode: String) {
+//        self.isExist = isExist
+//        self.name = name
+//        self.startDate = startDate
+//        self.endDate = endDate
+//        self.users = users
+//        self.usingRoles = usingRoles
+//        self.travelCode = travelCode
+//    }
+//
+//    func saveTravel(isExist: Bool, name: String, startDate: Date, endDate: Date, usingRoles: [Role]) {
+//        self.isExist = true
+//        self.name = name
+//        self.startDate = startDate
+//        self.endDate = endDate
+//        self.usingRoles = usingRoles
+//    }
+//}
+
+
+struct User {
+    var name: String
+    var myRole: Role?
+    var toDoList: [ToDoList]?
+}
+
+extension User {
+    static var myself = User(name: "오션", myRole: nil, toDoList: nil)
 }
 
 struct Role {
     var name: String
     var info: [String]
     var guide: [String]
+    var introducingSentence: String
+    var isChecked: Bool
 }
 
-struct User {
-    var name: String
-    var myRole: Role
-    var toDoList: [String]
+struct ToDoList {
+    var id: Int
+    var title: String
+    var isChecked: Bool
 }
+
--- TROL/TROL/TROLApp.swift
+++ TROL/TROL/TROLApp.swift
@@ -13,7 +13,7 @@ struct TROLApp: App {
 
     var body: some Scene {
         WindowGroup {
-            EmptyMyTravelView()
+            TabBarView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
